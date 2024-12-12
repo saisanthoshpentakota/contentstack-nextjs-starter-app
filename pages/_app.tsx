f48c261c760560882f8832d507ac57fb9873cea8
saisanthoshpentakota <saisanthosh.pentakota@contentstack.com>
2024-12-12 15:07:31 +0530

    Remove somethigng

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,28 @@
-import App from 'next/app';
-import Head from 'next/head';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import Layout from '../components/layout';
-import { getHeaderRes, getFooterRes, getAllEntries } from '../helper';
 import 'nprogress/nprogress.css';
 import '../styles/third-party.css';
 import '../styles/style.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 import '@contentstack/live-preview-utils/dist/main.css';
-import { Props } from "../typescript/pages";
 
+import Head from 'next/head';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+import { Props } from '../typescript/pages';
+import Home from './';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp(props: Props) {
-  const { Component, pageProps, header, footer, entries } = props;
-  const { page, posts, archivePost, blogPost } = pageProps;
-
   const metaData = (seo: any) => {
     const metaArr = [];
     for (const key in seo) {
       if (seo.enable_search_indexing) {
         metaArr.push(
           <meta
-            name={
-              key.includes('meta_')
-                ? key.split('meta_')[1].toString()
-                : key.toString()
-            }
+            name={key.includes('meta_') ? key.split('meta_')[1].toString() : key.toString()}
             content={seo[key].toString()}
             key={key}
           />
@@ -39,45 +31,24 @@ function MyApp(props: Props) {
     }
     return metaArr;
   };
-  const blogList: any = posts?.concat(archivePost);
+
   return (
     <>
       <Head>
-        <meta
-          name='application-name'
-          content='Contentstack-Nextjs-Starter-App'
-        />
-        <meta charSet='utf-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta
-          name='viewport'
-          content='width=device-width,initial-scale=1,minimum-scale=1'
-        />
-        <meta name='theme-color' content='#317EFB' />
+        <meta name="application-name" content="Contentstack-Nextjs-Starter-App" />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
+        <meta name="theme-color" content="#317EFB" />
         <title>Contentstack-Nextjs-Starter-App</title>
-        {page?.seo && page.seo.enable_search_indexing && metaData(page.seo)}
       </Head>
-      <Layout
-        header={header}
-        footer={footer}
-        page={page}
-        blogPost={blogPost}
-        blogList={blogList}
-        entries={entries}
-      >
-        <Component {...pageProps} />
-      </Layout>
+      <Home />
     </>
   );
 }
 
 MyApp.getInitialProps = async (appContext: any) => {
-  const appProps = await App.getInitialProps(appContext);
-  const header = await getHeaderRes();
-  const footer = await getFooterRes();
-  const entries = await getAllEntries();
-
-  return { ...appProps, header, footer, entries };
+  return {};
 };
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { onEntryChange } from '../contentstack-sdk';
-import RenderComponents from '../components/render-components';
-import { getPageRes } from '../helper';
-import Skeleton from 'react-loading-skeleton';
-import { Props, Context } from "../typescript/pages";
+import React from 'react';
 
-export default function Home(props: Props) {
+export default function Home() {
 
-  const { page, entryUrl } = props;
-
-  const [getEntry, setEntry] = useState(page);
-
-  async function fetchData() {
-    try {
-      const entryRes = await getPageRes(entryUrl);
-      if (!entryRes) throw new Error('Status code 404');
-      setEntry(entryRes);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    onEntryChange(() => fetchData());
-  }, []);
-
-  return getEntry ? (
-    <RenderComponents
-      pageComponents={getEntry.page_components}
-      contentTypeUid='page'
-      entryUid={getEntry.uid}
-      locale={getEntry.locale}
-    />
-  ) : (
-    <Skeleton count={3} height={300} />
-  );
+return <div> Hello World</div>
 }
 
-export async function getServerSideProps(context: Context) {
-  try {
-    const entryRes = await getPageRes(context.resolvedUrl);
-    return {
-      props: {
-        entryUrl: context.resolvedUrl,
-        page: entryRes,
-      },
-    };
-  } catch (error) {
-    return { notFound: true };
-  }
-}
